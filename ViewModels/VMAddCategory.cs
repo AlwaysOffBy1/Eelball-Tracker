daf85a7132bb8b9b5ae5f638300b78f496d719bd
AlwaysOffBy1 <alwaysoffby1@gmail.com>
2022-03-14 20:35:05 -0400

    investigating triggers for CategoryAddWindow. Created VMAddCategory to handle the load but it seems pointless for something so small

---
+++ ViewModels/VMAddCategory.cs
@@ -0,0 +1,43 @@
+﻿using EELBALL_TRACKER.Models;
+using EELBALL_TRACKER.Views;
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace EELBALL_TRACKER.ViewModels
+{
+    internal class VMAddCategory
+    {
+        public string Category;
+        public string Value;
+        public VMAddCategory() 
+        {
+            CategoryAddWindow x = new CategoryAddWindow();
+            x.Closing += PushDataAndClose;
+            x.btn_Cancel.Click +=PushDataAndClose; //need to add event to btn_Cancel and btn_Confirm. winforms loooks really easy right now
+            x.ShowDialog();
+
+        }
+        private void PushDataAndClose(object sender, CancelEventArgs e)
+        {
+            string source = sender.ToString();
+            switch (source)
+            {
+                case "Thrower":
+                    break;
+                case "Ball":
+                    break;
+                case "Contestant":
+                    break;
+                case "Result":
+                    break;
+            }
+            Task.Delay(1000);
+        }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using EELBALL_TRACKER.ViewModels;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -22,6 +23,8 @@ namespace EELBALL_TRACKER
     {
         public MainWindow()
         {
+            MessageBox.Show("DEBUG OPTION, remove in mainwindow.xaml.cs");
+            VMAddCategory x = new VMAddCategory();
             InitializeComponent();
         }
     }
